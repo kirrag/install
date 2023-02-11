8f4d0b622d71dec0944c3bd27a25e27715c292b3
Garrik Ten <ten@otux.ru>
2023-02-12 00:00:54 +0300

    Install version 1.0

--- src/Install.java
+++ src/Install.java
@@ -1,28 +1,45 @@
 import java.io.File;
 import java.io.IOException;
-
-import com.sun.source.tree.CatchTree;
+import java.lang.StringBuilder;
+import java.util.Date;
+import java.text.SimpleDateFormat;
+import java.io.FileWriter;
 
 public class Install {
 
+	public static StringBuilder log = new StringBuilder();
+
+	public static String getDate(Date date) {
+		String strDate = (new SimpleDateFormat("yyyy-MM-dd HH:mm:ss.SSS")).format(date);
+		return strDate;
+	}
+
 	public static void createDir(String baseDir, String dirName) {
 		File newDir = new File(baseDir + "/" + dirName);
-
+		String DirPathStr = " Каталог \"" + baseDir + "/" + newDir.getName() + "\"";
 		if (newDir.mkdir()) {
-			System.out.println("Каталог " + baseDir + "/" + newDir.getName() + " создан");
+			log.append(getDate(new Date()) + DirPathStr + " создан\n");
 		} else {
-			System.out.println("Каталог " + baseDir + "/" + newDir.getName() + " не создан");
+			if (newDir.exists()) {
+				log.append(getDate(new Date()) + DirPathStr + " уже существует\n");
+			} else {
+				log.append(getDate(new Date()) + DirPathStr + " не создан\n");
+			}
 		}
 	}
 
 	public static void createFile(String baseDir, String fileName) {
 		File newFile = new File(baseDir + "/" + fileName);
+		String FilePathStr = " Файл \"" + baseDir + "/" + newFile.getName() + "\"";
 		try {
-			if (newFile.createNewFile()) {
-				System.out.println("Файл " + baseDir + "/" + newFile.getName() + " создан");
+			if (newFile.exists()) {
+				log.append(getDate(new Date()) + FilePathStr + " файл уже существует\n");
+			} else {
+				newFile.createNewFile();
+				log.append(getDate(new Date()) + FilePathStr + " создан\n");
 			}
 		} catch (IOException e) {
-			System.out.println(e.getMessage());
+			log.append(getDate(new Date()) + FilePathStr + " не создан: " + e.getMessage() + "\n");
 		}
 	}
 
@@ -55,5 +72,14 @@ public class Install {
 		// 5. В директории temp создайте файл temp.txt.
 		baseDir = gamesDir + "/" + "temp";
 		createFile(baseDir, "temp.txt");
+		
+		// Запись логов
+		try (FileWriter fw = new FileWriter(baseDir + "/temp.txt", true)) {
+			fw.write(log.toString());
+			fw.flush();
+			System.out.println("Лог создан: " + baseDir + "/temp.txt");
+		} catch (IOException e) {
+			System.out.println("Лог не создан " + e.getMessage());
+		}
 	}
 }
